090186f30e1549c7586edb3c228132bc80e7b1d9
nikfrv <xionixx2016@gmail.com>
2025-04-16 13:54:39 +0300

    Initial commit

---
+++ src/main/java/com/example/templategenerator/TemplateGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.templategenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/templategenerator/TemplateGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.templategenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
